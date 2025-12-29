0dc6f86789f997c65d82e77b5fb41e3022a73319
Parbat Karki <karkiparbat9@gmail.com>
2025-12-29 09:56:52 +0545

    first commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import authRouter from './src/routes/auth.js'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/auth', authRouter)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
+
+export default app
